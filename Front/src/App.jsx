701d4a86fc76c2a8bc3e77143c7ebd1585d30e3c
adamsharifc <97190119+adamsharifc@users.noreply.github.com>
2024-03-17 08:54:00 +0400

    configured shadcn and tailwind

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-
+import HomePage from './pages/HomePage/HomePage'
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <HomePage />
     </>
   )
 }
---
+++ Front/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import "./HomePage.css"
+import { Button } from "@/components/ui/button";
+
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+        <h1>Home Page</h1>
+        <Button>Click me</Button>
+        </div>
+    )
+}
+
+export default HomePage;
