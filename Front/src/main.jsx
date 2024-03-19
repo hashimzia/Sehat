4c5d204ee7d7763111beb09f21cf01c6349913db
adamsharifc <97190119+adamsharifc@users.noreply.github.com>
2024-03-20 01:13:10 +0400

    added react-router

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import AppointmentBookingPage from './pages/AppointmentBookingPage/AppointmentBookingPage'
+import HomePage from './pages/HomePage/HomePage'
+import LoginPage from './pages/LoginPage/LoginPage'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/appointment-booking",
+    element: <AppointmentBookingPage />
+  }
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
